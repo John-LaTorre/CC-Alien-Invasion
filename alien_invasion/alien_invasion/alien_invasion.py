a385e1c7de2a0d8712271750f485c66e463bec5e
John LaTorre <johnjlatorre@gmail.com>
2019-11-13 21:01:26 -0500

    Create game screen and establish settings class

--- alien_invasion/alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion/alien_invasion.py
@@ -1,1 +1,27 @@
-This took forever because git control on Visual Studio is the worst thing in the world
+import sys
+
+import pygame
+from settings import Settings
+
+def run_game():
+    #Initialize pygame, settings and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #Start the main loop
+    while True:
+
+        #Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #Redraw the screen during each pass through loop
+        screen.fill(ai_settings.bg_color)
+
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
